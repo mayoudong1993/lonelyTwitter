451b5f09ce6bbab9137eb7fcbc73ada8ba115cb8
Youdong Ma <youdong@cs.ualberta.ca>
2015-01-15 16:43:47 -0700

    lab2

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -49,6 +49,16 @@ public class LonelyTwitterActivity extends Activity {
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
+		User u = new Readers();
+		
+		ArrayList<User> array = new ArrayList<User>();
+		
+		try {
+			u.setUsername("Joe");
+		} catch (IOException e) {
+			
+		}
+		
 		super.onStart();
 		String[] tweets = loadFromFile();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
